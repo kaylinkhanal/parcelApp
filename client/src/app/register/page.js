745aa8f07bb3264aefcd31e5f9c814c5699b9d35
Kyalima <kyalima@Kyalima.local>
2024-01-19 08:37:50 +0545

    added external css

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -3,17 +3,22 @@ import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import {Input} from "@nextui-org/react";
+import styles from './styles.module.css'
 const SignupForm = () => {
    const SignupSchema = Yup.object().shape({
+     email: Yup.string().email('Invalid email').required('Required'),
    email: Yup.string().email('Invalid email').required('Required'),
  });
  
  const registerUser = async(values)=> {
-  await fetch('http://localhost:5000/register/',{
+  debugger;
+ const res=  await fetch('http://localhost:5000/register/',{
     method: 'POST',
     headers: {'Content-Type':'application/json' },
     body: JSON.stringify(values)
   })
+  const data = await res.json()
+  alert(data.msg)
  }
   const formik = useFormik({
     initialValues: {
@@ -29,7 +34,7 @@ const SignupForm = () => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form  className={styles.formfields} onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">phoneNumber</label>
       <Input 
        id="phoneNumber"
--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,6 +1,12 @@
 const User = require('../models/user')
 const registerNewUser = async(req,res)=>{
     try{
+        const existingUser = await User.findOne({phoneNumber: req.body.phoneNumber})
+        if(existingUser){
+            return res.status(403).json({
+                msg: "phone number already exist"
+            })
+        }
         await User.create(req.body)
         res.json({
             msg: "registered successfully"

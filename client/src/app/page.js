ea35e3a9a45aae04c79ee1183caa4b957290916a
Kyalima <kyalima@Kyalima.local>
2024-01-24 08:43:24 +0545

    add incerment logic

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,15 +3,18 @@ import React from 'react'
 import Nav from '@/components/navBar/page'
 import Layout from '@/components/layout/page'
 import { useSelector, useDispatch } from 'react-redux'
-// import {increment} from 
+import {increment} from '@/redux/reducerSlice/countSlice'
 const page = () => {
   const {count} = useSelector(state=>state.count)
+  const dispatch = useDispatch();
+  // 'Normal function call it increment()'
+  // 'Redux function  dispatch and call it dispatch(increment())'
 
   return (
     <div>
       <Layout>
        count is {count}
-       <button >Increment</button>
+       <button onClick={()=>dispatch(increment())}>Increment</button>
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">

eee617233b05f1d140cb1f1a071c0dc1b7340852
Biraj Thapa <birajthapa983@gmail.com>
2024-01-24 09:03:00 +0545

    decrement added

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Nav from '@/components/navBar/page'
 import Layout from '@/components/layout/page'
 import { useSelector, useDispatch } from 'react-redux'
-import {increment} from '@/redux/reducerSlice/countSlice'
+import {increment,decrement} from '@/redux/reducerSlice/countSlice'
 const page = () => {
   const {count} = useSelector(state=>state.count)
   const dispatch = useDispatch();
@@ -15,6 +15,7 @@ const page = () => {
       <Layout>
        count is {count}
        <button onClick={()=>dispatch(increment())}>Increment</button>
+       <button onClick={()=>dispatch(decrement())}>decrement</button>
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">

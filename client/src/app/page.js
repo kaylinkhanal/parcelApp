a6943307f2718a945f0174963639987b4072eb72
SushilKushwaha <sushilsinha733@gmail.com>
2024-01-25 21:39:04 +0545

    change the field name completed

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -28,7 +28,19 @@ const page = () => {
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Ship, manage, track, deliver</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">So much goes into running your business. Take some time to recharge and read. Get guidance on everything from creating a returns strategy to improving sustainability.</p>
     </div>
-    <div className="flex flex-wrap -m-4 text-center">
+    <h1 className="sm:text-3xl text-2xl text-center font-medium title-font mb-4 text-gray-900 ">Get In Touch</h1>
+    
+    <div className="flex flex-wrap -m-4 text-center">    
+    <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+            <path d="M3 18v-6a9 9 0 0118 0v6"></path>
+            <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+          </svg>
+          <h2 className="title-font font-medium text-2xl text-gray-900">Customer Support</h2>
+          <p className="leading-relaxed">Inquire about due deliveries</p>
+        </div>
+      </div>
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
@@ -46,20 +58,11 @@ const page = () => {
             <circle cx="9" cy="7" r="4"></circle>
             <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
           </svg>
-          <h2 className="title-font font-medium text-3xl text-gray-900">fff</h2>
-          <p className="leading-relaxed">REDIRECT A PACKAGE</p>
+          <h2 className="title-font font-medium text-2xl text-gray-900">Careers</h2>
+          <p className="leading-relaxed">Explore opportunities to join our team</p>
         </div>
       </div>
-      <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
-        <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M3 18v-6a9 9 0 0118 0v6"></path>
-            <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
-          </svg>
-          <h2 className="title-font font-medium text-3xl text-gray-900">74</h2>
-          <p className="leading-relaxed">STORE HOURS AND SERVICES</p>
-        </div>
-      </div>
+      
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
--- client/src/components/footer/page.js
+++ client/src/components/footer/page.js
@@ -8,7 +8,7 @@ const page = () => {
         <img src='/parcellogo.png' width="80" height="90"  />
         <span className="ml-3 text-xl">ParcelApp</span>
       </a>
-      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 ParcelApp
+      <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Copyright © 2024 ParcelApp
         
       </p>
       <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">

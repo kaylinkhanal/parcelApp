823451de5224a2c160e7c7096c499e19d2f77bed
Sushma Acharya <sushmaacharya345@gmail.com>
2024-02-19 09:43:38 +0545

    styled login as register css

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,71 +1,159 @@
+// 'use client'
+// import React from 'react';
+// import { useFormik } from 'formik';
+// import * as Yup from 'yup';
+// import {Input,Button} from "@nextui-org/react";
+// import { addUserDetails } from '@/redux/reducerSlice/userSlice';
+// import {  toast } from 'react-toastify';
+// import Layout from '@/components/layout/page'
+// import { useRouter } from 'next/navigation'
+// import { useDispatch } from 'react-redux';
+// const SignInForm = () => {
+//   const router = useRouter()
+//   const dispatch =useDispatch()
+//    const SignInSchema = Yup.object().shape({
+//    phoneNumber: Yup.string().required('Required'),
+//  });
+ 
+//  const loginUser = async(values)=> {
+//  const res=  await fetch(`http://localhost:${process.env.NEXT_PUBLIC_API_URL}/login/`,{
+//     method: 'POST',
+//     headers: {'Content-Type':'application/json' },
+//     body: JSON.stringify(values)
+//   })
+//   const data = await res.json()
+//   if(res.status == 200) {
+//     dispatch(addUserDetails(data))
+//     router.push('/home')
+//   } else {
+//     toast(data.msg)
+
+//   }
+  
+//  }
+//   const formik = useFormik({
+//     initialValues: {
+//       phoneNumber: '',
+//       password: '',
+//     },
+//     validationSchema:SignInSchema,
+//     onSubmit: values => {
+//       loginUser(values)
+//     },
+//   });
+
+//   return (
+//     <Layout>
+//     <form  className='p-24' onSubmit={formik.handleSubmit}>
+//       <h2>Sign In</h2>
+//       <Input 
+//        id="phoneNumber"
+//        label="phoneNumber"
+//        name="phoneNumber"
+//        type="text"
+//        onChange={formik.handleChange}
+//        value={formik.values.phoneNumber}
+//       />
+//         {formik?.errors.phoneNumber}
+//          <Input 
+//        id="password"
+//        name="password"
+//        type="password"
+//        onChange={formik.handleChange}
+//        value={formik.values.password}
+//       label="password" />
+//       <Button type="submit">Submit</Button>
+//     </form>
+//   </Layout>
+//   );
+// };
+
+// export default SignInForm
 'use client'
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import {Input,Button} from "@nextui-org/react";
-import { addUserDetails } from '@/redux/reducerSlice/userSlice';
-import {  toast } from 'react-toastify';
-import Layout from '@/components/layout/page'
-import { useRouter } from 'next/navigation'
+import { Input, Button } from "@nextui-org/react";
+import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
-const SignInForm = () => {
-  const router = useRouter()
-  const dispatch =useDispatch()
-   const SignInSchema = Yup.object().shape({
-   phoneNumber: Yup.string().required('Required'),
- });
- 
- const loginUser = async(values)=> {
- const res=  await fetch(`http://localhost:${process.env.NEXT_PUBLIC_API_URL}/login/`,{
-    method: 'POST',
-    headers: {'Content-Type':'application/json' },
-    body: JSON.stringify(values)
-  })
-  const data = await res.json()
-  if(res.status == 200) {
-    dispatch(addUserDetails(data))
-    router.push('/home')
-  } else {
-    toast(data.msg)
+import { addUserDetails } from '@/redux/reducerSlice/userSlice';
+import { toast } from 'react-toastify';
+import Layout from '@/components/layout/page';
+import styles from './styles.module.css'
 
-  }
-  
- }
+
+const SignInForm = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const SignInSchema = Yup.object().shape({
+    phoneNumber: Yup.string().required('Required'),
+    password: Yup.string().required('Required'),
+  });
+
+  const loginUser = async (values) => {
+    const res = await fetch(`http://localhost:${process.env.NEXT_PUBLIC_API_URL}/login/`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    if (res.status === 200) {
+      dispatch(addUserDetails(data));
+      router.push('/home');
+    } else {
+      toast(data.msg);
+    }
+  };
+
   const formik = useFormik({
     initialValues: {
       phoneNumber: '',
       password: '',
     },
-    validationSchema:SignInSchema,
-    onSubmit: values => {
-      loginUser(values)
+    validationSchema: SignInSchema,
+    onSubmit: (values) => {
+      loginUser(values);
     },
   });
 
   return (
     <Layout>
-    <form  className='p-24' onSubmit={formik.handleSubmit}>
-      <h2>Sign In</h2>
-      <Input 
-       id="phoneNumber"
-       label="phoneNumber"
-       name="phoneNumber"
-       type="text"
-       onChange={formik.handleChange}
-       value={formik.values.phoneNumber}
-      />
-        {formik?.errors.phoneNumber}
-         <Input 
-       id="password"
-       name="password"
-       type="password"
-       onChange={formik.handleChange}
-       value={formik.values.password}
-      label="password" />
-      <Button type="submit">Submit</Button>
-    </form>
-  </Layout>
+      <div className="flex items-center justify-center">
+      <form  className={styles.formfields} onSubmit={formik.handleSubmit}>
+        {/* <form className="w-full max-w-md p-8 bg-gray-100 rounded-lg" onSubmit={formik.handleSubmit}> */}
+          <h2 className="text-xl m-6 font-bold text-center text-orange-500">Sign In</h2>
+          <div className="mb-4">
+            <Input
+              id="phoneNumber"
+              label="Phone Number"
+              name="phoneNumber"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.phoneNumber}
+            />
+            {formik.errors.phoneNumber && <p className="text-red-500">{formik.errors.phoneNumber}</p>}
+          </div>
+          <div className="mb-6">
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              label="Password"
+            />
+            {formik.errors.password && <p className="text-red-500">{formik.errors.password}</p>}
+          </div>
+          <div className="flex justify-center">
+            <Button type="submit" className="w-full bg-orange-500 hover:bg-orange-600">
+              Sign In
+            </Button>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 };
 
-export default SignInForm
+export default SignInForm;
